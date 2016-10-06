676f7068405b1fc8a0e32c2052c76802557ed02c
Keith Davis <keith.davis@zunisoft.com>
2016-10-06 12:22:07 -0700

    Removed API key for Github push.

--- Camping Directory/Constants.swift
+++ Camping Directory/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Constants {
     static let svcCampgroundURL = "http://api.amp.active.com/camping/campgrounds"
     static let svcCampgroundDetailURL = "http://api.amp.active.com/camping/campground/details"
-    static let svcCampgroundAPIKey = "f7rk6put964u42x5jb32wsj4"
+    static let svcCampgroundAPIKey = "XXXXXXXXXXXXXXXXXXX"
     
     static let stateList = ["Alabama",
                             "Alaska",
